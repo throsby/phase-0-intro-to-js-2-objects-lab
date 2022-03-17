9b27b02148c2d560f0bfc4f36cd1ad0a1e454688
Throsby Wells <throsbywells@gmail.com>
2022-03-16 22:08:40 -0400

    commit tree bc511e54557efdc652edff455701da3210a6c036

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sam", 
+    streetAddress: "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return employee[key] = value
+}
+
+console.log(updateEmployeeWithKeyAndValue(employee,"name","Throsby"))
